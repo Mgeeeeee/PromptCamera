c20a57ea0026d75d27bfdd7ba3a55ad6fc7b31a6
Mgeeeeee <610814162@qq.com>
2025-12-24 12:52:08 +0800

    fix: Improve primary button styling and tooltip text
    Adds a subtle shadow to the primary button for better visual definition and corrects a minor typo in the Chinese tooltip text.

--- App.tsx
+++ App.tsx
@@ -108,7 +108,7 @@ const App: React.FC = () => {
   };
 
   const commonBtnClass = "w-12 h-12 rounded-[1.2rem] bg-white/5 border border-white/5 flex items-center justify-center active:scale-90 transition-all shrink-0 disabled:opacity-10";
-  const primaryBtnClass = "w-12 h-12 rounded-[1.2rem] bg-white border border-white flex items-center justify-center active:scale-90 transition-all shrink-0 disabled:opacity-20";
+  const primaryBtnClass = "w-12 h-12 rounded-[1.2rem] bg-white border border-white flex items-center justify-center active:scale-90 transition-all shrink-0 disabled:opacity-20 shadow-[0_0_20px_rgba(255,255,255,0.1)]";
   const dangerBtnClass = "w-12 h-12 rounded-[1.2rem] bg-red-500/10 border border-red-500/20 flex items-center justify-center active:scale-90 transition-all shrink-0";
 
   return (
@@ -164,7 +164,7 @@ const App: React.FC = () => {
               IMAGE
             </h1>
             <p className="text-white/40 text-[10px] md:text-xs font-bold uppercase tracking-[0.1em] text-center animate-pulse whitespace-nowrap">
-              请耐心等待不要退离开界面
+              请耐心等待不要离开界面
             </p>
           </div>
         </div>
@@ -204,9 +204,8 @@ const App: React.FC = () => {
               </button>
 
               <button onClick={runAI} disabled={!capturedImage || isGenerating} className={primaryBtnClass} title="Generate">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-6 h-6 text-black">
-                  <path d="M14.633 7.189l2.547-2.547a.75.75 0 111.06 1.06l-2.546 2.548a.75.75 0 11-1.061-1.06zm-9.207 9.208a.75.75 0 010 1.06l-2.547 2.547a.75.75 0 01-1.06-1.06l2.546-2.547a.75.75 0 011.061 0zM7.19 14.633a.75.75 0 011.06 0l2.547 2.547a.75.75 0 11-1.06 1.06L7.19 15.694a.75.75 0 010-1.061zm9.208-9.207a.75.75 0 011.061 0l2.547 2.546a.75.75 0 11-1.06 1.06l-2.547-2.546a.75.75 0 010-1.06zM12 7.5a4.5 4.5 0 110 9 4.5 4.5 0 010-9zm0 1.5a3 3 0 100 6 3 3 0 000-6z" />
-                  <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-4.14a.75.75 0 10-1.06-1.06l-6.5 6.5a.75.75 0 101.06 1.06l6.5-6.5z" clipRule="evenodd" />
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-6 h-6 text-black">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
                 </svg>
               </button>
             </>
@@ -237,7 +236,7 @@ const App: React.FC = () => {
 
               <button onClick={runAI} disabled={isGenerating} className={commonBtnClass} title="Regenerate">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2.5} stroke="currentColor" className="w-5 h-5 text-white/50">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 12c0-1.232-.37-2.377-1-3.33M4.5 12c0 1.232.37 2.377 1 3.33m13.5-3.33a7.5 7.5 0 11-13.5 0m13.5 0H21m-16.5 0H3" />
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                 </svg>
               </button>
             </>
